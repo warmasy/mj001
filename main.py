e8bf5900621dbe4a86dd8c0333df6bea029cceda
warmasy <3139822307@qq.com>
2026-05-03 20:25:54 +0800

    first

--- main.py
+++ main.py
@@ -87,6 +87,6 @@ async def db_health():
         }
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
